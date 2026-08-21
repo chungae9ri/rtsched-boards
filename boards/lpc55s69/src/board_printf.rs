b2a362e59a30da459cdee1701044927d2818fc0c
chungae9ri <chungae9ri@gmail.com>
2026-08-20 23:42:55 -0400

    Rtsched update (#62)
    * added sched_minimal and board_shell applications
    
    * updated rtsched submodule

--- boards/lpc55s69/src/board_printf.rs
+++ boards/lpc55s69/src/board_printf.rs
@@ -2,6 +2,7 @@ use core::sync::atomic::{AtomicU8, Ordering};
 
 use rtt_target::rprint;
 
+#[cfg(feature = "board-rt")]
 use crate::drivers::uart;
 
 use rtsched::Mutex;
@@ -16,14 +17,17 @@ static STDIO_MTX: Mutex<u32> = Mutex::new(0);
 
 static STDOUT: AtomicU8 = AtomicU8::new(Stdout::Uart as u8);
 
+#[allow(dead_code)]
 pub fn set_stdout(stdout: Stdout) {
     STDOUT.store(stdout as u8, Ordering::Release);
 }
 
+#[allow(dead_code)]
 pub fn set_stdout_uart() {
     set_stdout(Stdout::Uart);
 }
 
+#[allow(dead_code)]
 pub fn set_stdout_rtt() {
     set_stdout(Stdout::Rtt);
 }
@@ -41,16 +45,18 @@ pub fn board_printf(message: &str) {
     };
 
     match stdout() {
+        #[cfg(feature = "board-rt")]
         Stdout::Uart if uart_ready() => uart_write_str(message),
-        Stdout::Uart => rprint!("{}", message),
-        Stdout::Rtt => rprint!("{}", message),
+        _ => rprint!("{}", message),
     }
 }
 
+#[cfg(feature = "board-rt")]
 fn uart_ready() -> bool {
     crate::UART_READY.load(Ordering::Acquire)
 }
 
+#[cfg(feature = "board-rt")]
 fn uart_write_str(message: &str) {
     for &byte in message.as_bytes() {
         uart::write_byte(byte);
